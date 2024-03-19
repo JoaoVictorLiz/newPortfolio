b485e263b532458d830154f220a6d889362cc8b6
João Victor Liz <joaovictorlizsilveira@gmail.com>
2024-03-19 09:36:18 -0300

    Vercel Test

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./footer.css"
 import {FaGithub} from 'react-icons/fa'
-import {BsInstagram, BsLinkedin, BsGi} from 'react-icons/bs'
+import {BsInstagram, BsLinkedin} from 'react-icons/bs'
 
 
 const Footer = () => {
@@ -17,7 +17,6 @@ const Footer = () => {
         <li><a href="#">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#experience">Experience</a></li>
-        {/* <li><a href="#services">Services</a></li> */}
         <li><a href="#portfolio">Portfolio</a></li>
         <li><a href="#contact">Contact</a></li>
       </ul>
