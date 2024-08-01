8411281bd1a997a3db1255788c5c5dc63f3f0b24
João Victor Liz <joaovictorlizsilveira@gmail.com>
2024-08-01 23:26:04 +0100

    contact info update

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -40,7 +40,7 @@ const Contact = () => {
          <article className='contact__option'>
           <BsWhatsapp className='contact__option-icon'/>
           <h4>WhatsApp</h4>
-          <h5>+55 51 99552-8628</h5>
+          <h5>+353 087 189 0030</h5>
           <a href="https://api.whatsapp.com/send?phone=5551995528628" target="_blank">Send a message</a>
          </article>
         </div>
