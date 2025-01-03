17cbbfecc9ba5cc6c44a4b08b48379ff1638018f
João Victor Liz <joaovictorlizsilveira@gmail.com>
2025-01-03 21:20:27 +0000

    img change

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./about.css"
-import ME from "../../assets/EU4.jpeg"
+import ME from "../../assets/EU.jpg"
 import {FaAward} from "react-icons/fa"
 import {VscFolderLibrary} from 'react-icons/vsc'
 
